b9161d2a78b6b563922532612ff21e3d3d0204c2
vpasias <52623837+vpasias@users.noreply.github.com>
2021-09-17 16:49:26 +0300

    Update monitoring-install.sh

--- monitoring-install.sh
+++ monitoring-install.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
+# https://computingforgeeks.com/setup-prometheus-and-grafana-on-kubernetes/
 
+git clone https://github.com/prometheus-operator/kube-prometheus.git && cd kube-prometheus
+
+kubectl create -f manifests/setup
+
+kubectl get ns monitoring
+
+sleep 300
+
+kubectl get pods -n monitoring
+
+kubectl create -f manifests/
+
+kubectl get pods -n monitoring
+
+kubectl get svc -n monitoring
+
+# kubectl delete --ignore-not-found=true -f manifests/ -f manifests/setup
