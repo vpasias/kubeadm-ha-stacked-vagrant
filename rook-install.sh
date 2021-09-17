e5bdf4c12add4ae8ae31aaedf5728f993859d02b
vpasias <52623837+vpasias@users.noreply.github.com>
2021-09-17 16:25:47 +0300

    Update rook-install.sh

--- rook-install.sh
+++ rook-install.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
+# https://itnext.io/using-rook-on-a-k3s-cluster-8a97a75ba25e
 
+sudo fdisk -l | grep 'Disk /dev/sd*'
+
+git clone --branch v1.7.3 https://github.com/rook/rook.git && cd rook/cluster/examples/kubernetes/ceph
+
+kubectl create -f crds.yaml -f common.yaml -f operator.yaml
+kubectl create -f cluster.yaml
+
+kubectl apply -f ./csi/rbd/storageclass.yaml
+
+kubectl patch storageclass local-path -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"false"}}}'
+
+kubectl patch storageclass rook-ceph-block -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
