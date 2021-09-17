19b840781cd474ad2b5973347a0b25bca1fe928e
vpasias <52623837+vpasias@users.noreply.github.com>
2021-09-17 17:07:30 +0300

    Create kubevirt-install.sh

---
+++ kubevirt-install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# https://opensource.com/article/20/9/vms-kubernetes-kubevirt
+# http://kubevirt.io/user-guide/operations/installation/
+
+sudo virt-host-validate qemu
+
+export KUBEVIRT_VERSION="v0.45.0"
+
+wget https://github.com/kubevirt/kubevirt/releases/download/${KUBEVIRT_VERSION}/kubevirt-operator.yaml
+kubectl apply -f kubevirt-operator.yaml
+kubectl create configmap kubevirt-config -n kubevirt --from-literal debug-useEmulation=true
+wget https://github.com/kubevirt/kubevirt/releases/download/${KUBEVIRT_VERSION}/kubevirt-cr.yaml
+kubectl apply -f kubevirt-cr.yaml
+kubectl get pods -n kubevirt
