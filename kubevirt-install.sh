2042f4e35f424ab76eb24b8ee09ea7b12d6c0b44
vpasias <52623837+vpasias@users.noreply.github.com>
2021-09-25 18:41:55 +0300

    Update kubevirt-install.sh

--- kubevirt-install.sh
+++ kubevirt-install.sh
@@ -18,9 +18,7 @@ kubectl apply -f kubevirt-cr.yaml
 kubectl get pods -n kubevirt
 
 VERSION=$(kubectl get kubevirt.kubevirt.io/kubevirt -n kubevirt -o=jsonpath="{.status.observedKubeVirtVersion}")
-ARCH=$(uname -s | tr A-Z a-z)-$(uname -m | sed 's/x86_64/amd64/') || windows-amd64.exe
-echo ${ARCH}
-curl -L -o virtctl https://github.com/kubevirt/kubevirt/releases/download/${VERSION}/virtctl-${VERSION}-${ARCH}
+curl -L -o virtctl https://github.com/kubevirt/kubevirt/releases/download/${VERSION}/virtctl-${VERSION}-linux-amd64
 chmod +x virtctl
 sudo install virtctl /usr/local/bin
 virtctl version
