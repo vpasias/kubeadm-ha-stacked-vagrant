4e3e4b7f6d478d89b16dfb03e2dc6afa1d716454
vpasias <52623837+vpasias@users.noreply.github.com>
2021-09-28 11:34:42 +0300

    Create kupenstack-install.sh

---
+++ kupenstack-install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# https://github.com/Kupenstack/kupenstack/tree/main/config/demo
+
+# See nodes names and change accordingly the relative Configmap fields at file: "kupenstack-controller-manager.yaml"
+kubectl get nodes
+
+kubectl apply -f https://raw.githubusercontent.com/vpasias/kubeadm-ha-stacked-vagrant/master/kupenstack-controller-manager.yaml
+
+kubectl get pods -n kupenstack-control
+
+kubectl apply -f https://raw.githubusercontent.com/vpasias/kubeadm-ha-stacked-vagrant/master/vm-sample.yaml
+
+kubectl get virtualmachines
+
+kubectl get vm -o wide
+
+kubectl get networks
+
+kubectl get flavors
+
+kubectl get images
+
+kubectl get keypairs -o wide
+
+kubectl describe ns default
